25aed6f773d40e978b6af7898875c3a3a96d65fc
lylllcc <lylllcc@outlook.com>
2017-07-08 13:20:21 +0800

    init

---
+++ src/main/java/com/mvc/upc/SupermarketApplication.java
@@ -0,0 +1,12 @@
+package com.mvc.upc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketApplication.class, args);
+	}
+}
