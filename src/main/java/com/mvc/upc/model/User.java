7196c73e060737877f5c6b78e81cc589c58fffbe
lylllcc <lylllcc@outlook.com>
2017-07-08 17:11:15 +0800

    jwt model

--- src/main/java/com/mvc/upc/model/User.java
+++
@@ -1,7 +0,0 @@
-package com.mvc.upc.model;
-
-/**
- * Created by wanghaojun on 2017/7/8.
- */
-public class User {
-}
--- src/main/java/com/mvc/upc/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mvc.upc.repository;
-
-/**
- * Created by wanghaojun on 2017/7/8.
- */
-public class UserRepository {
-}
