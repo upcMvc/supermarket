b872a894ce632cae39e1ffc5fa9993c96bf9ba01
lylllcc <lylllcc@outlook.com>
2017-07-10 20:44:31 +0800

    添加权限接口

--- src/main/java/com/mvc/upc/service/AddAuthority.java
+++ src/main/java/com/mvc/upc/service/AddAuthority.java
@@ -1,7 +1,35 @@
 package com.mvc.upc.service;
 
+import com.mvc.upc.security.model.Authority;
+import com.mvc.upc.security.model.AuthorityRepository;
+import com.mvc.upc.security.model.User;
+import com.mvc.upc.security.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by lylllcc on 2017/7/10.
  */
+@Service
 public class AddAuthority {
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void addAuthority(User user, Authority authority){
+        User user1 = userRepository.findFirstByUsername(user.getUsername());
+        if(user1 == null){
+            user1 = userRepository.save(user);
+        }
+        List<Authority> list = user1.getAuthorities();
+        list.add(authority);
+        user1.setAuthorities(list);
+        userRepository.save(user1);
+    }
+
 }
