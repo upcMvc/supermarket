717bc4c73cfeecf0d285b2c35555fd7818f9491a
lylllcc <lylllcc@outlook.com>
2017-07-10 15:10:13 +0800

    添加快速生成token接口

--- src/main/java/com/mvc/upc/security/controller/DevTocken.java
+++ src/main/java/com/mvc/upc/security/controller/DevTocken.java
@@ -1,5 +1,10 @@
 package com.mvc.upc.security.controller;
 
+import com.mvc.upc.security.service.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DevTocken {
-    
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    @GetMapping("/dev/user")
+    public String genToken(){
+        final UserDetails userDetails = userDetailsService.loadUserByUsername("dev");
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return token;
+    }
 }
