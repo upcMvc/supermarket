d3e80f0ffa96750c196e71797f95fc343a3c1734
DESKTOP-NQNCHBC\chenzifeng <704734862@qq.com>
2017-07-11 19:50:00 +0800

    修改经纬判断的bug

--- src/main/java/com/mvc/upc/security/config/WebSecurityConfig.java
+++ src/main/java/com/mvc/upc/security/config/WebSecurityConfig.java
@@ -84,6 +84,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user/**").permitAll()
                 .antMatchers("/dev/**").permitAll()
                 .antMatchers("/goods/findAll").permitAll()
+                .antMatchers("/ShopRecord/**").permitAll()
                 .anyRequest().authenticated();
 
         // Custom JWT based security filter
