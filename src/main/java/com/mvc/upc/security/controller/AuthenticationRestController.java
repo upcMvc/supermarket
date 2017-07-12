303f46794a56436cf56b1dd25912520fd7064089
lylllcc <lylllcc@outlook.com>
2017-07-12 14:05:14 +0800

    完善注册

--- src/main/java/com/mvc/upc/security/controller/AuthenticationRestController.java
+++ src/main/java/com/mvc/upc/security/controller/AuthenticationRestController.java
@@ -1,6 +1,6 @@
 package com.mvc.upc.security.controller;
 
-import com.mvc.upc.security.service.JwtAuthenticationRequest;
+import com.mvc.upc.dto.JsonMes;
 import com.mvc.upc.security.service.JwtAuthenticationResponse;
 import com.mvc.upc.security.service.JwtTokenUtil;
 import com.mvc.upc.security.service.JwtUser;
@@ -8,19 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mobile.device.Device;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by lylllcc on 2017/7/9.
@@ -37,13 +35,18 @@ public class AuthenticationRestController {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @RequestMapping(value = "/auth", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest, Device device) throws AuthenticationException {
-
-
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST)
+    public Object createAuthenticationToken(String username, String password, Device device) throws AuthenticationException {
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        if(bCryptPasswordEncoder.matches(password,userDetails.getPassword()) == false){
+            return new JsonMes(-1,"用户名或密码错误");
+        }
         final String token = jwtTokenUtil.generateToken((JwtUser) userDetails, device);
-        return ResponseEntity.ok(new JwtAuthenticationResponse(token));
+        Map<Object,Object> map = new HashMap<>();
+        map.put("user",userDetails);
+        map.put("tocken",token);
+        return map;
     }
 
     @RequestMapping(value = "/refresh", method = RequestMethod.GET)
